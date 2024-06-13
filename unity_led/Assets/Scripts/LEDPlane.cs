f6215e19f59699ca4789e2548f7d8d2345274e0d
Filip <fifav@seznam.cz>
2024-06-13 15:44:08 -0700

    Unity Led project stage simulation

---
+++ unity_led/Assets/Scripts/LEDPlane.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LEDPlane : MonoBehaviour
+{
+    [SerializeField] private SerialArrayReader serialArrayReader;
+    [SerializeField] private Texture2D ledColors;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < serialArrayReader.Colors.Length; i++)
+        {
+            ledColors.SetPixel(i, 0, serialArrayReader.Colors[i]);
+        }
+
+        ledColors.Apply();
+    }
+}
